ea9c233e698e583453124284fc08ecba1a0173fa
xumihao33 <xumihao33@gmail.com>
2026-04-18 22:35:24 +0800

    queue

---
+++ acwing4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int main()
+{
+    // vector<int>x={1,2,3,4,5};
+    // cout<<*(x.end()-1)<<endl;
+    queue<int>x;    
+    return 0;   
+}
