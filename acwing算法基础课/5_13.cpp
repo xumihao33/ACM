63e45690c6d0f0a860ac0530b200f2dc8a831234
xumihao33 <xumihao33@gmail.com>
2026-05-13 21:18:46 +0800

    离散化

---
+++ acwing算法基础课/5_13.cpp
@@ -0,0 +1,18 @@
+//双指针，位运算，离散化，区间合并
+#include <iostream>
+using namespace std;
+vector<int>alls;
+sort(alls.begin(),alls.end());
+alls.erase(unique(alls.begin(),alls.end()),alls.end());
+int find()
+{
+    int l=-1;
+    int right=alls.size();
+    while(l+1<r)
+    {
+        int mid=l+r>>1;
+        if(alls[mid]<x)l=mid;
+        else r=mid;
+    }
+    return r+1;
+}
